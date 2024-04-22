2d565f5ac277e612b0d2e445d8202b5e8ce95fd8
KulBoy6122 <sarthakkulkarni612@gmail.com>
2024-04-22 20:14:14 +0530

    Auth and WebHooks added

---
+++ convex/auth.config.ts
@@ -0,0 +1,11 @@
+
+const authConfig =  {
+    providers: [
+      {
+        domain: "https://native-pony-20.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+  export default authConfig;
